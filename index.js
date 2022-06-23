17816d420a02aeaab16f04146e921717ad1c4aab
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-06-23 00:36:59 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+import chalk from 'chalk';
+import fs from 'fs';
+
+/**
+ * Função para executar o readFile da biblioteca fs
+ * @param filePath
+ */
+function readFile(filePath) {
+    const encoding = 'utf-8';
+    fs.readFile(filePath, encoding, callBackFunctionWithErrorHandling);
+}
+
+/**
+ * Função de callback para o fs.readfile, onde o '_' significa que
+ * se deseja ignorar o primeiro parametro, neste caso o erro
+ * @param _
+ * @param data
+ */
+function callBackFunctionWithoutErrorHandling(_, data) {
+    console.log(chalk.green(data));
+}
+
+/**
+ * Função de callback para o fs.readfile com tratamento de erro
+ * @param err
+ * @param data
+ */
+function callBackFunctionWithErrorHandling(err, data) {
+    if (err) {
+        errorHandling(err);
+    }
+    console.log(chalk.green(data));
+}
+
+/**
+ * Função para lançar erro
+ * @param err
+ */
+function errorHandling(err) {
+    throw new Error(chalk.red.bold(err));
+}
+
+/**
+ * Função que executa o fs.readFile usando promises. Significa
+ * que vai executar o código que está em then() apenas após a
+ * execução de fs.readFile, sendo passado uma função de tratamento
+ * de erro para o catch()
+ * @param filePath
+ */
+function readFileUsingPromises(filePath) {
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(filePath, encoding)
+        .then((data) => console.log(chalk.green(data)))
+        .catch(errorHandling);
+}
+
+/**
+ * Função que executa o fs.readFile usando as notações async e await
+ * @param filePath
+ * @returns {Promise<void>}
+ */
+async function readFilesUsingAsyncAwait(filePath) {
+    const encoding = 'utf-8';
+    try {
+        const result = await fs.promises.readFile(filePath, encoding);
+        console.log(extractLinks(result));
+    } catch (err) {
+        errorHandling(err);
+    } finally {
+        console.log(chalk.yellow('Operation Completed'))
+    }
+}
+
+/**
+ * Função que retorna links e suas chaves extraídos de um texto
+ * @param text
+ */
+function extractLinks(text) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].\S*)\)/gm;
+    const resultsArray = [];
+    let temp = regex.exec(text);
+    for(temp; temp !== null; temp = regex.exec(text)) {
+        resultsArray.push({ [temp[1]] : temp[2] });
+    }
+    return resultsArray;
+}
+
+readFilesUsingAsyncAwait('./files/texto1.md');
