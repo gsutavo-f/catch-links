432d18c8490ba84ca26d03829d2e915f148c5d74
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-06-26 16:55:04 -0300

    Implementando multiplos testes

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,18 @@
 import {readFilesUsingAsyncAwait} from '../index.js';
 
-test('expect to be function', () => {
-   expect(typeof readFilesUsingAsyncAwait).toBe('function');
+const arrayResult = [
+   {
+      FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+   }
+];
+
+describe('readFilesUsingAsyncAwait::', () => {
+   it('expect to be function', () => {
+      expect(typeof readFilesUsingAsyncAwait).toBe('function');
+   });
+   it('must return array result', async () => {
+      const result = await readFilesUsingAsyncAwait('C:/Users/gu_fe/Desktop/programs/node/lib_markdown/' +
+         'test/files/texto1.md');
+      expect(result).toEqual(arrayResult);
+   });
 });
